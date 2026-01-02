c57bdd5a24f0bbae1edab08f9cc7a9e423acd1ce
Koratahiu~ <haidermontage191@gmail.com>
2026-01-03 00:56:11 +0300

    fix

--- adv_optm/__init__.py
+++ adv_optm/__init__.py
@@ -7,6 +7,7 @@ from .optim import (
     Lion_Prodigy_adv,
     Muon_adv,
     AdaMuon_adv,
+    SignSGD,
 )
 
 __all__ = [
@@ -18,6 +19,7 @@ __all__ = [
     "Lion_Prodigy_adv",
     "Muon_adv",
     "AdaMuon_adv",
+    "SignSGD",
 ]
 
 __version__ = "2.1.dev5"
